c855968e3141cb146e363a9e1d15fdefe7a423b4
Lukasz <Lukasz@DESKTOP-UJMQ8CP>
2026-02-06 17:31:13 +0100

    Add project files.

---
+++ GymTrackerApp/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using DynamicInputsApp.ViewModels;
+
+namespace GymTrackerApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ GymTrackerApp/Models/InputItem.cs
@@ -0,0 +1,7 @@
+﻿namespace DynamicInputsApp.Models;
+
+public class InputItem
+{
+    public string? ExerciseName { get; set; }
+    public string? ExerciseValue { get; set; }
+}
