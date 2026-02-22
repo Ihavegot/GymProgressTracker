4e489bc96892f4253099575905e2aaebc9245c7d
Ihavegot <lukasz.bocianski1112@gmail.com>
2026-02-22 15:04:56 +0100

    feat: add exercise types picker for AddExercisePage

--- GymTrackerApp/MVVM/Models/Exercise.cs
+++ GymTrackerApp/MVVM/Models/Exercise.cs
@@ -10,5 +10,6 @@ namespace GymTrackerApp.MVVM.Models
         public string ExerciseName { get; set; } = "";
         public string RepsOrTime { get; set; } = "0";
         public string Weight { get; set; } = "0";
+        public ExerciseType Type { get; set; }
     }
 }
---
+++ GymTrackerApp/MVVM/ViewModels/ExerciseTypeViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymTrackerApp.MVVM.ViewModels
+{
+    public class ExerciseTypeViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<ExerciseType> ExerciseTypes { get; }
+
+        private ExerciseType _selectedExercise;
+        public ExerciseType SelectedExercise
+        {
+            get => _selectedExercise;
+            set
+            {
+                if (_selectedExercise != value)
+                {
+                    _selectedExercise = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ExerciseTypeViewModel()
+        {
+            ExerciseTypes = new ObservableCollection<ExerciseType>(Enum.GetValues(typeof(ExerciseType)).Cast<ExerciseType>());
+            SelectedExercise = ExerciseType.FBW;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
